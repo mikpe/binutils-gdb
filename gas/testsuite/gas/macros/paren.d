93ed8165bafa576c8f40ac96db05cb0b8b0f4bdb
Mikael Pettersson <mikpelinux@gmail.com>
2023-07-09 16:10:27 +0200

    PDP-10 gas support (testsuite xfails)
    gas/testsuite/
    	* gas/macros/irp.d: xfail on PDP-10.
    	* gas/macros/paren.d: Likewise.
    	* gas/macros/repeat.d: Likewise.
    	* gas/macros/rept.d: Likewise.
    	* gas/macros/test2.d: Likewise.
    	* gas/macros/vararg.d: Likewise.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gas/testsuite/gas/macros/paren.d
+++ gas/testsuite/gas/macros/paren.d
@@ -1,6 +1,8 @@
 #as: -f
 #objdump: -s -j .data
 #name parenthesized macro arguments
+# pdp10 allocates two octets per nonet so the offsets are off and the contents more spaced apart
+#xfail: pdp10-*-*
 
 .*: .*
 
