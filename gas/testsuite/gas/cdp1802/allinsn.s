88c9e2a1bac5abb8407a4f9746d88a0dd5314bfe
Mikael Pettersson <mikpelinux@gmail.com>
2024-03-17 16:11:40 +0100

    CDP1802 gas support (new files)
    gas/
    	* config/tc-cdp1802.c: New.
    	* config/tc-cdp1802.h: New.
    
    gas/testsuite/
    	* gas/cdp1802/allinsn.d: New.
    	* gas/cdp1802/allinsn.l: New.
    	* gas/cdp1802/allinsn.s: New.
    	* gas/cdp1802/cdp1802.exp: New.
    	* gas/cdp1802/hilo.d: New.
    	* gas/cdp1802/hilo.s: New.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

---
+++ gas/testsuite/gas/cdp1802/allinsn.s
@@ -0,0 +1,120 @@
+	.text
+	adc
+	adci	0x42
+	add
+	adi	0x42
+	and
+	ani	0x42
+	b1	0x42
+	b2	0x42
+	b3	0x42
+	b4	0x42
+	bci	0x42
+	bdf	0x42
+	bge	0x42
+	bl	0x42
+	bm	0x42
+	bn1	0x42
+	bn2	0x42
+	bn3	0x42
+	bn4	0x42
+	bnf	0x42
+	bnq	0x42
+	bnz	0x42
+	bpz	0x42
+	bq	0x42
+	br	0x42
+	bxi	0x42
+	bz	0x42
+	cid
+	cie
+	daci	0x42
+	dadc
+	dadd
+	dadi	0x42
+	dbnz	7, 0x4711
+	dec	7
+	dis
+	dsav
+	dsbi	0x42
+	dsm
+	dsmb
+	dsmi	0x42
+	dtc
+	etq
+	gec
+	ghi	7
+	glo	7
+	idl
+	inc	7
+	inp	7
+	irx
+	lbdf	0x4711
+	lbnf	0x4711
+	lbnq	0x4711
+	lbnz	0x4711
+	lbq	0x4711
+	lbr	0x4711
+	lbz	0x4711
+	lda	7
+	ldc
+	ldi	0x42
+	ldn	7
+	ldx
+	ldxa
+	lsdf
+	lsie
+	lskp
+	lsnf
+	lsnq
+	lsnz
+	lsq
+	lsz
+	mark
+	nbr
+	nlbr
+	nop
+	or
+	ori	0x42
+	out	7
+	phi	7
+	plo	7
+	req
+	ret
+	rldi	7, 0x4711
+	rlxa	7
+	rnx	7
+	rshl
+	rshr
+	rsxd	7
+	sav
+	scal	7
+	scm1
+	scm2
+	sd
+	sdb
+	sdbi	0x42
+	sdi	0x42
+	sep	7
+	seq
+	sex	7
+	shl
+	shlc
+	shr
+	shrc
+	skp
+	sm
+	smb
+	smbi	0x42
+	smi	0x42
+	spm1
+	spm2
+	sret	7
+	stm
+	stpc
+	str	7
+	stxd
+	xid
+	xie
+	xor
+	xri	0x42
