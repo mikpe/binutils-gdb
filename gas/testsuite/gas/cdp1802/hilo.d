8ecdea9905f07246468e924ca447dba5e151fb26
Mikael Pettersson <mikpelinux@gmail.com>
2024-03-17 16:11:40 +0100

    CDP1802 gas support (new files)
    gas/
    	* config/tc-cdp1802.c: New.
    	* config/tc-cdp1802.h: New.
    
    gas/testsuite/
    	* gas/cdp1802/allinsn.d: New.
    	* gas/cdp1802/allinsn.l: New.
    	* gas/cdp1802/allinsn.s: New.
    	* gas/cdp1802/cdp1802.exp: New.
    	* gas/cdp1802/hilo.d: New.
    	* gas/cdp1802/hilo.s: New.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

---
+++ gas/testsuite/gas/cdp1802/hilo.d
@@ -0,0 +1,12 @@
+#objdump: -dr
+
+.*\.o:     file format elf32-cdp1802
+
+
+Disassembly of section .text:
+
+00000000 <foo-0x4>:
+   0:	f8 00       	ldi	0
+			1: R_CDP1802_HI8	.text\+0x5
+   2:	f8 05       	ldi	0x5
+			3: R_CDP1802_LO8	.text\+0x5
