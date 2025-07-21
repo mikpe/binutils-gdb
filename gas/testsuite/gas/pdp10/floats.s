52abee76526f36cb80ab1ffa399b5f70a831b0be
Mikael Pettersson <mikpelinux@gmail.com>
2025-07-21 17:56:54 +0200

    PDP-10 gas support (new files)
    gas/
    	* config/tc-pdp10.c: New.
    	* config/tc-pdp10.h: New.
    
    gas/testsuite/
    	* gas/pdp10/basic.d: New.
    	* gas/pdp10/basic.l: New.
    	* gas/pdp10/basic.s: New.
    	* gas/pdp10/data.d: New.
    	* gas/pdp10/data.s: New.
    	* gas/pdp10/floats.d: New.
    	* gas/pdp10/floats.s: New.
    	* gas/pdp10/pdp10.exp: New.
    	* gas/pdp10/reloc.d: New.
    	* gas/pdp10/reloc.s: New.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

---
+++ gas/testsuite/gas/pdp10/floats.s
@@ -0,0 +1,12 @@
+	.data
+	.p2align 3
+	.pdp10_ffloat 153
+	.pdp10_ffloat -153
+	.pdp10_dfloat 153
+	.pdp10_dfloat -153
+	.pdp10_dfloat 262144.0000038147
+	.pdp10_dfloat -262144.0000038147
+	.pdp10_gfloat 153
+	.pdp10_gfloat -153
+	.pdp10_gfloat 262144.0000038147
+	.pdp10_gfloat -262144.0000038147
